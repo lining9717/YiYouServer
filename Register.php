bb34b3145d4503f2455fdc28cacc69d4718e495d
lining <980994630@qq.com>
2018-06-27 23:36:27 +0800

    修改注册，增加所有字段

--- Register.php
+++ Register.php
@@ -8,7 +8,7 @@
 
 require_once('Connect.php');
 $username = $_POST['username'];
-$password = $_POST['passowrd'];
+$password = $_POST['password'];
 $tel = $_POST['tel'];
 
 function isTelUsed($conn,$telnumber){
@@ -23,13 +23,13 @@ function isTelUsed($conn,$telnumber){
 }
 
 if(!isTelUsed($conn,$tel)){
-    $sql_insert = "insert into user(uNickname,uTelephone,uSex,".
-        "uHeadPhoto,uIntroduce,uIsGuide,uStars,uPassword) ".
-        "values($username,$tel,'男','默认','让旅行更简单','否',5,$password)";
+    $sql_insert = "insert into user".
+        "(uNickname,uTelephone,uSex,uHeadPhoto,uIntroduce,uIsGuide,uStars,uPassword,uGuideId,uCollectGuideId,uCollectEssayId) ".
+        "values('$username','$tel','男','默认','让旅行更简单','否',5,'$password',0,0,0);";
     if ($conn->query($sql_insert) === TRUE) {
         Response::json(1,"注册成功","");
     } else {
-        Response::json(0,"注册失败，服务端错误","");
+        Response::json(0,"注册失败，服务端错误:".$conn->error,"");
     }
 }else{
     Response::json(0,"手机号码已被使用","");
