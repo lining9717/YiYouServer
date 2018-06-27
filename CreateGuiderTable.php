01496621ce18fb2de3f7ecaea037c80790bf1d40
lining <980994630@qq.com>
2018-06-27 14:36:55 +0800

    modify some errors

--- CreateGuiderTable.php
+++ CreateGuiderTable.php
@@ -26,7 +26,7 @@ gServerCity varchar(50) not null,
 gStars int not null
 )";
 
-if ($conn->query($user_table_sql) === TRUE) {
+if ($conn->query($guider_table_sql) === TRUE) {
     echo "User table created successfully";
 } else {
     echo "Fail to create user table " . $conn->error;
