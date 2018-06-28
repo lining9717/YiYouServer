4091f4d4309a35f178466355b34bd5638038ee49
lining <980994630@qq.com>
2018-06-28 10:07:26 +0800

    增加了头像注册及默认头像

--- Login.php
+++ Login.php
@@ -6,34 +6,36 @@
  * Time: 15:13
  */
 
-require_once ('Connect.php');
+require_once('Connect.php');
 
 $tel = $_POST['tel'];
 $password = $_POST['password'];
 
 $sql_login = "select * from user where uTelephone = '$tel'";
-$result = $conn->query($sql);
 
-if ($result->num_rows > 0){
+$result = $conn->query($sql_login);
+if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
-    if($password == $row['uPassword']){
+    if ($password == $row['uPassword']) {
         $data = array(
-            "nickname"=>$row['uNickname'],
-            "telephone"=>$row['uTelephone'],
-            "sex"=>$row['uSex'],
-            "headphoto"=>$row['uHeadPhoto'],
-            "introduce"=>$row['uIntroduce'],
-            "isguide"=>$row['uIsGuide'],
-            "guideid"=>$row['uGuideId'],
-            "collectguideid"=>$row['uCollectGuideId'],
-            "collecteassyid"=>$row['uCollectEssayId'],
-            "star"=>$row['uStars'],
-            "password"=>$row['uPassword']
+            "nickname" => $row['uNickname'],
+            "telephone" => $row['uTelephone'],
+            "sex" => $row['uSex'],
+            "headphoto" => $row['uHeadPhoto'],
+            "introduce" => $row['uIntroduce'],
+            "isguide" => $row['uIsGuide'],
+            "guideid" => $row['uGuideId'],
+            "collectguideid" => $row['uCollectGuideId'],
+            "collecteassyid" => $row['uCollectEssayId'],
+            "star" => $row['uStars'],
+            "password" => $row['uPassword']
         );
-        Response::json(1,"登录成功",$data);
-    }else{
-        Response::json(0,"密码错误","");
+        Response::json(1, "登录成功", $data);
+    } else {
+        Response::json(0, "密码错误", "");
     }
+} else {
+    Response::json(0, "账户不存在", "");
 }
 $conn->close();
 
--- Register.php
+++ Register.php
@@ -10,6 +10,7 @@ require_once('Connect.php');
 $username = $_POST['username'];
 $password = $_POST['password'];
 $tel = $_POST['tel'];
+$headImage = $_POST['imageInfo'];
 
 function isTelUsed($conn,$telnumber){
     $sql_check = "select uTelephone from user";
@@ -23,13 +24,35 @@ function isTelUsed($conn,$telnumber){
 }
 
 if(!isTelUsed($conn,$tel)){
-    $sql_insert = "insert into user".
-        "(uNickname,uTelephone,uSex,uHeadPhoto,uIntroduce,uIsGuide,uStars,uPassword,uGuideId,uCollectGuideId,uCollectEssayId) ".
-        "values('$username','$tel','男','默认','让旅行更简单','否',5,'$password',0,0,0);";
-    if ($conn->query($sql_insert) === TRUE) {
-        Response::json(1,"注册成功","");
-    } else {
-        Response::json(0,"注册失败，服务端错误:".$conn->error,"");
+    if($headImage == 'default'){
+        $imagepath = "http://118.89.18.136/YiYou/YiYouImg/Avatar/default.png";
+        $sql_insert = "insert into user".
+            "(uNickname,uTelephone,uSex,uHeadPhoto,uIntroduce,uIsGuide,uStars,uPassword,uGuideId,uCollectGuideId,uCollectEssayId) ".
+            "values('$username','$tel','男','$imagepath','让旅行更简单','否',5,'$password',0,0,0)";
+        if ($conn->query($sql_insert) === TRUE) {
+            Response::json(1,"注册成功","");
+        } else {
+            Response::json(0,"注册失败1，服务端错误:".$conn->error,"");
+        }
+    }else{
+        if($_FILES["file"]["error"]>0){
+            Response::json(0,"头像文件传输错误：". $_FILES["file"]["error"],"");
+        }else{
+            $newfile= time().rand(1,1000).substr($_FILES["file"]["name"],strrpos($_FILES["file"]["name"],"."));
+            $imagepath= "http://118.89.18.136/YiYou/YiYouImg/Avatar/".$newfile;
+            if(move_uploaded_file($_FILES["file"]["tmp_name"], "Avatar/".$newfile)){
+                $sql_insert = "insert into user".
+                    "(uNickname,uTelephone,uSex,uHeadPhoto,uIntroduce,uIsGuide,uStars,uPassword,uGuideId,uCollectGuideId,uCollectEssayId) ".
+                    "values('$username','$tel','男','$imagepath','让旅行更简单','否',5,'$password',0,0,0)";
+                if ($conn->query($sql_insert) === TRUE) {
+                    Response::json(1,"注册成功","");
+                } else {
+                    Response::json(0,"注册失败2，服务端错误:".$conn->error,"");
+                }
+            }else{
+                Response::json(0,"头像文件移动错误","");
+            }
+        }
     }
 }else{
     Response::json(0,"手机号码已被使用","");
