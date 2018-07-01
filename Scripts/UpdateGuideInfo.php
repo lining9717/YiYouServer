c6f9f05a9097ce08b02e655c92a2aa1d5f467367
lining <980994630@qq.com>
2018-07-01 16:18:54 +0800

    增加更新导游信息

---
+++ Scripts/UpdateGuideInfo.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lining
+ * Date: 2018/7/1
+ * Time: 10:54
+ */
+
+require_once('Connect.php');
+
+$guideIDNumber = $_POST['IDNumber'];
+$guideServerCity = $_POST['servercity'];
+
+$update_guide_sql = "update guide set
+                     gServerCity = '$serveServerCity'
+                     where gIDNumber = '$guideIDNumber'";
+if ($conn->query($update_guide_sql) === TRUE) {
+    Response::json(1,"修改成功","");
+} else {
+    Response::json(0,"修改失败1，服务端错误:".$conn->error,"");
+}
+$conn->close();
