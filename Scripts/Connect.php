300093d741a17bb0313d42a337c8db01827e4567
lining <980994630@qq.com>
2018-07-02 10:12:06 +0800

    修改返回中文json

--- Scripts/Connect.php
+++ Scripts/Connect.php
@@ -14,7 +14,13 @@ class Response{
             'data'=>$data
         );
         //输出json
-        echo json_encode($result);
+        $json = json_encode($result);
+        echo preg_replace_callback('/\\\\u([0-9a-f]{4})/i',
+            create_function(
+                '$matches',
+                'return mb_convert_encoding(pack("H*", $matches[1]), "UTF-8", "UCS-2BE");'
+            ),
+            $json);
         exit;
     }
 }
