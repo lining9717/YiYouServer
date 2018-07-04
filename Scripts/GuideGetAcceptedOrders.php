13a67945c3811d6e466d4a074f9c19edba272d55
lining <980994630@qq.com>
2018-07-05 00:01:04 +0800

    向导获取accepted订单

--- Scripts/GuideGetAcceptedOrders.php
+++ Scripts/GuideGetAcceptedOrders.php
@@ -17,29 +17,33 @@ if($result_get_guideId->num_rows>0){
     $guideId = $row_get_guideId['gId'];
     $sql_get_orders_id = "select gogOrderId from getorderguides where gogGuideId = $guideId";
     $result_get_orders_id = $conn->query($sql_get_orders_id);
-    $data=array();
-    while($row_get_orders_id = $result_get_orders_id->fetch_assoc()){
-        $orderId = $row_get_orders_id['gogOrderId'];
-        $sql_get_order_info = "select * from `order` where oId = $orderId";
-        $result_order_info = $conn->query($sql_get_order_info);
-        if($result_order_info->num_rows>0){
-            $row = $result_order_info->fetch_assoc();
-            $info=array(
-                "orderID"=>$row['oId'],
-                "status"=>$row['oStatus'],
-                "place"=>$row['oPlace'],
-                "date"=>$row['oDate'],
-                "numberOfPeople"=>$row['oNumberOfPeople'],
-                "note"=>$row["oDescription"],
-                "userNickname"=>$row['oUserNickname']
-            );
-            array_push($data,$info);
+    if($result_get_orders_id->num_rows>0){
+        $data=array();
+        while($row_get_orders_id = $result_get_orders_id->fetch_assoc()){
+            $orderId = $row_get_orders_id['gogOrderId'];
+            $sql_get_order_info = "select * from `order` where oId = $orderId";
+            $result_order_info = $conn->query($sql_get_order_info);
+            if($result_order_info->num_rows>0){
+                $row = $result_order_info->fetch_assoc();
+                $info=array(
+                    "orderID"=>$row['oId'],
+                    "status"=>$row['oStatus'],
+                    "place"=>$row['oPlace'],
+                    "date"=>$row['oDate'],
+                    "numberOfPeople"=>$row['oNumberOfPeople'],
+                    "note"=>$row["oDescription"],
+                    "userNickname"=>$row['oUserNickname']
+                );
+                array_push($data,$info);
+            }
         }
-    }
-    if(empty($data)){
-        Response::json(0,"No accepted orders","");
+        if(empty($data)){
+            Response::json(0,"No accepted orders","");
+        }else{
+            Response::json(1,"Get accepted orders success",$data);
+        }
     }else{
-        Response::json(1,"Get accepted orders success",$data);
+        Response::json(0,"No accepted orders","");
     }
 }else{
     Response::json(0,"Get guide Id fail, server error: ".$conn->error,"");
