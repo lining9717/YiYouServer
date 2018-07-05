4418e6e3d4ab0085ffa48e2d50b9c55ab0d03c0f
lining <980994630@qq.com>
2018-07-05 15:43:10 +0800

    用户发表博文

---
+++ Bowen/Connect.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lining
+ * Date: 2018/6/27
+ * Time: 14:41
+ */
+
+class Response{
+    public static function json($code,$message="",$data=array()){
+        $result=array(
+            'code'=>$code,
+            'message'=>$message,
+            'data'=>$data
+        );
+        //输出json
+        echo json_encode($result);
+        exit;
+    }
+}
+
+$servername = "localhost";
+$username = "root";
+$password = "Abc12345";
+$dbname = "YiYou";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+// 检测连接
+if ($conn->connect_error) {
+    Response::json(0,"Connect database fail:".$conn->connect_error,"");
+    die("连接失败: " . $conn->connect_error);
+}
+
+
+
+
+
---
+++ Bowen/ReleaseBowen.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lining
+ * Date: 2018/7/5
+ * Time: 15:01
+ */
+
+require_once('Connect.php');
+
+$userTel = str_replace('"','', $_POST['tel']);
+$title = $_POST['title'];
+$content = $_POST['content'];
+$time = str_replace('"','', $_POST['time']);
+$imageInfo = str_replace('"','', $_POST['imageInfo']);
+
+$sql_get_user_info = "select * from user where uTelephone = '$userTel'";
+$result_get_user_info = $conn->query($sql_get_user_info);
+if($result_get_user_info->num_rows>0){
+    $row_get_user_info = $result_get_user_info->fetch_assoc();
+    $userId = $row_get_user_info['uId'];
+    $userNickname = $row_get_user_info['uNickname'];
+    if($imageInfo == 'no'){
+        $sql_release_bowen = "insert into bowen".
+                             "(bUserId,bUserNickName,bTitle,bBody,bZanNumber,bCommentId,bCollectNumber,bTime,bImage) ".
+                             "values($userId,'$userNickname','$title','$content',0,0,0,'$time','no')";
+        if ($conn->query($sql_release_bowen) === TRUE) {
+            Response::json(1,"Release bowen success","");
+        } else {
+            Response::json(0,"Release fail 1 in server ".$conn->error,"");
+        }
+    }else{
+        if($_FILES["file"]["error"]>0){
+            Response::json(0,"Image file error：". $_FILES["file"]["error"],"");
+        }else{
+            $filename = $_FILES["file"]["name"];
+            $newfile= time().rand(1,1000).substr($filename,strrpos($filename,"."));
+            $imagepath= "http://118.89.18.136/YiYou/image/".$newfile;
+            if(move_uploaded_file($_FILES["file"]["tmp_name"], "image/".$newfile)){
+                $sql_release_bowen = "insert into bowen".
+                    "(bUserId,bUserNickName,bTitle,bBody,bZanNumber,bCommentId,bCollectNumber,bTime,bImage) ".
+                    "values($userId,'$userNickname','$title','$content',0,0,0,'$time','$imagepath')";
+                if ($conn->query($sql_release_bowen) === TRUE) {
+                    Response::json(1,"Release bowen success","");
+                } else {
+                    Response::json(0,"Release fail 2 in server ".$conn->error,"");
+                }
+            }else{
+                Response::json(0,"Image file move error","");
+            }
+        }
+    }
+}else{
+    Response::json(0,"Get user information error: ".$conn->error,"");
+}
+$conn->close();
+
