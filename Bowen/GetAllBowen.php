d869ef85544dc858a8aa318b42161d9fdc6106a1
lining <980994630@qq.com>
2018-07-05 22:54:07 +0800

    修改获取所有博文的错误

--- Bowen/GetAllBowen.php
+++ Bowen/GetAllBowen.php
@@ -11,31 +11,23 @@ require_once('Connect.php');
 $sql_get_all_bowen = "select * from bowen";
 $result_get_all_bowen = $conn->query($sql_get_all_bowen);
 if($result_get_all_bowen->num_rows>0){
-    $row_get_all_bowen = $result_get_all_bowen->fetch_assoc();
-    $data = array(
-        "userId"=>$row_get_all_bowen['bUserId'],
-        "userNickname"=>$row_get_all_bowen['bUserNickName'],
-        "title"=>$row_get_all_bowen['bTitle'],
-        "content"=>$row_get_all_bowen['bBody'],
-        "ZanNumber"=>$row_get_all_bowen['bZanNumber'],
-        "commentID"=>$row_get_all_bowen['bCommentId'],
-        "collectedNumber"=>$row_get_all_bowen['bCollectNumber'],
-        "time"=>$row_get_all_bowen['bTime'],
-        "image"=>$row_get_all_bowen['bImage']
-    );
+    $data = array();
+    while($row_get_all_bowen = $result_get_all_bowen->fetch_assoc()){
+        $info = array(
+            "userId"=>$row_get_all_bowen['bUserId'],
+            "userNickname"=>$row_get_all_bowen['bUserNickName'],
+            "title"=>$row_get_all_bowen['bTitle'],
+            "content"=>$row_get_all_bowen['bBody'],
+            "ZanNumber"=>$row_get_all_bowen['bZanNumber'],
+            "commentID"=>$row_get_all_bowen['bCommentId'],
+            "collectedNumber"=>$row_get_all_bowen['bCollectNumber'],
+            "time"=>$row_get_all_bowen['bTime'],
+            "image"=>$row_get_all_bowen['bImage']
+        );
+        array_push($data,$info);
+    }
     Response::json(1,"Get all bowen success",$data);
 }else{
-    $data = array(
-        "userId"=>"empty",
-        "userNickname"=>"empty",
-        "title"=>"empty",
-        "content"=>"empty",
-        "ZanNumber"=>"empty",
-        "commentID"=>"empty",
-        "collectedNumber"=>"empty",
-        "time"=>"empty",
-        "image"=>"empty"
-    );
-    Response::json(0,"No bowen",$data);
+    Response::json(0,"No bowen","");
 }
 $conn->close();
