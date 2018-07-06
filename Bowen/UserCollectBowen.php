fc033814f7429701b1e4ff8e482865f612baa473
lining <980994630@qq.com>
2018-07-06 14:13:40 +0800

    用户收藏博文

--- Bowen/UserCollectBowen.php
+++ Bowen/UserCollectBowen.php
@@ -19,10 +19,16 @@ if($result_get_userId->num_rows>0){
     $sql_get_bowen_info = "select * from bowen where bId = $bowenId";
     $result_get_bowen_info = $conn->query($sql_get_bowen_info);
     if($result_get_bowen_info->num_rows>0){
-
-
-
-
+        $row_get_bowen_info= $result_get_bowen_info->fetch_assoc();
+        $bowenTitle= $row_get_bowen_info['bTitle'];
+        $sql_collect_bowen = "insert into collectedessay".
+                              "(ceBowenId,ceUserId,ceBowenTitle) ".
+                              "values($bowenId,$userId,'$bowenTitle')";
+        if ($conn->query($sql_collect_bowen) === TRUE) {
+            Response::json(1,"Collect success","");
+        } else {
+            Response::json(0,"Collect fail in server ".$conn->error,"");
+        }
     }else{
         Response::json(0,"Get bowen information error: ".$conn->error,"");
     }
