592c35b2f1a501b59b6199913d91ee40696e0fe7
lining <980994630@qq.com>
2018-07-06 15:00:56 +0800

    收藏博文时，博文收藏数加一；同时对收藏博文和向导时检测是否已收藏

--- Bowen/UserCollectBowen.php
+++ Bowen/UserCollectBowen.php
@@ -11,31 +11,56 @@ require_once ('Connect.php');
 $tel = $_POST['tel'];
 $bowenId = $_POST['bowenId'];
 
+function isCollected($userId,$bowenId,$conn){
+    $sql = "select * from collectedessay where ceBowenId=$bowenId and ceUserId = $userId";
+    $result = $conn->query($sql);
+    if($result->num_rows>0){
+        return true;
+    }
+    return false;
+}
+
 $sql_get_userId = "select uId from user where uTelephone = '$tel'";
 $result_get_userId = $conn->query($sql_get_userId);
 if($result_get_userId->num_rows>0){
     $row_get_userId = $result_get_userId->fetch_assoc();
     $userId = $row_get_userId['uId'];
-    $sql_get_bowen_info = "select * from bowen where bId = $bowenId";
-    $result_get_bowen_info = $conn->query($sql_get_bowen_info);
-    if($result_get_bowen_info->num_rows>0){
-        $row_get_bowen_info= $result_get_bowen_info->fetch_assoc();
-        $bowenTitle= $row_get_bowen_info['bTitle'];
-        $bowenUserId = $row_get_bowen_info['bUserId'];
-        if($bowenUserId != $userId){
-            $sql_collect_bowen = "insert into collectedessay".
-                "(ceBowenId,ceUserId,ceBowenTitle) ".
-                "values($bowenId,$userId,'$bowenTitle')";
-            if ($conn->query($sql_collect_bowen) === TRUE) {
-                Response::json(1,"Collect success","");
-            } else {
-                Response::json(0,"Collect fail in server ".$conn->error,"");
+    if(!isCollected($userId,$bowenId,$conn)){
+        $sql_get_bowen_info = "select * from bowen where bId = $bowenId";
+        $result_get_bowen_info = $conn->query($sql_get_bowen_info);
+        if($result_get_bowen_info->num_rows>0){
+            $row_get_bowen_info= $result_get_bowen_info->fetch_assoc();
+            $bowenTitle= $row_get_bowen_info['bTitle'];
+            $bowenUserId = $row_get_bowen_info['bUserId'];
+            if($bowenUserId != $userId){
+                $sql_collect_bowen = "insert into collectedessay".
+                    "(ceBowenId,ceUserId,ceBowenTitle) ".
+                    "values($bowenId,$userId,'$bowenTitle')";
+                if ($conn->query($sql_collect_bowen) === TRUE) {
+                    $sql_update_collect = "select bCollectNumber from bowen where bId = $bowenId";
+                    $result_update_collect = $conn->query($sql_update_collect);
+                    $row_update_collect = $result_update_collect->fetch_assoc();
+                    $collectNumber = (int)($row_update_collect['bCollectNumber']);
+                    $collectNumber++;
+                    $sql_update_collectNumber = "update bowen set 
+                         bCollectNumber = $collectNumber
+                         where bId = $bowenId";
+                    if ($conn->query($sql_update_collectNumber) === TRUE) {
+                        Response::json(1,"Collect success","");
+                    } else {
+                        Response::json(0,"Update collect number fail in server ".$conn->error,"");
+                    }
+                } else {
+                    Response::json(0,"Collect fail in server ".$conn->error,"");
+                }
+            }else{
+                Response::json(0,"You can collect your own blog ","");
             }
         }else{
-            Response::json(0,"You can collect your own blog ","");
+            Response::json(0,"Get bowen information error: ".$conn->error,"");
         }
     }else{
-        Response::json(0,"Get bowen information error: ".$conn->error,"");
+        Response::json(0,"You have collected this blog","");
     }
 }else{
     Response::json(0,"Account error","");
--- Scripts/UserCollectGuide.php
+++ Scripts/UserCollectGuide.php
@@ -11,35 +11,48 @@ require_once('Connect.php');
 $guideNumber = $_POST['guideNumber'];
 $userTel = $_POST['tel'];
 
+function isCollected($userId,$guideId,$conn){
+    $sql = "select * from collectedguide where cgGuideId=$guideId and cgUserId = $userId";
+    $result = $conn->query($sql);
+    if($result->num_rows>0){
+        return true;
+    }
+    return false;
+}
+
 $sql_get_userId = "select uId from user where uTelephone = '$userTel'";
 $result_get_userId = $conn->query($sql_get_userId);
 if($result_get_userId->num_rows>0){
     $row_get_userId = $result_get_userId->fetch_assoc();
     $userId = $row_get_userId['uId'];
-    $sql_get_guide_info = "select * from guide where gGuideNumber = '$guideNumber'";
-    $result_get_guide_info = $conn->query($sql_get_guide_info);
-    if($result_get_guide_info->num_rows>0){
-        $row_get_guide_info = $result_get_guide_info->fetch_assoc();
-        $guideId = $row_get_guide_info['gId'];
-        $guideRealname = $row_get_guide_info['gRealName'];
-        $sql_get_guide_headicon = "select uHeadPhoto from user where uGuideId=$guideId";
-        $result_get_guide_headicon = $conn->query($sql_get_guide_headicon);
-        if($result_get_guide_headicon->num_rows>0){
-            $row_get_guide_headicon = $result_get_guide_headicon->fetch_assoc();
-            $guideHeadIcon = $row_get_guide_headicon['uHeadPhoto'];
-            $sql_collect = "insert into collectedguide".
-                           "(cgGuideId,cgUserId,cgGuideRealName,cgGuideHeadIcon) ".
-                           "values($guideId,$userId,'$guideRealname','$guideHeadIcon')";
-            if ($conn->query($sql_collect) === TRUE) {
-                Response::json(1,"Collect success","");
-            } else {
-                Response::json(0,"Collect fail in server ".$conn->error,"");
+    if(!isCollected($userId,$guideId,$conn)){
+        $sql_get_guide_info = "select * from guide where gGuideNumber = '$guideNumber'";
+        $result_get_guide_info = $conn->query($sql_get_guide_info);
+        if($result_get_guide_info->num_rows>0){
+            $row_get_guide_info = $result_get_guide_info->fetch_assoc();
+            $guideId = $row_get_guide_info['gId'];
+            $guideRealname = $row_get_guide_info['gRealName'];
+            $sql_get_guide_headicon = "select uHeadPhoto from user where uGuideId=$guideId";
+            $result_get_guide_headicon = $conn->query($sql_get_guide_headicon);
+            if($result_get_guide_headicon->num_rows>0){
+                $row_get_guide_headicon = $result_get_guide_headicon->fetch_assoc();
+                $guideHeadIcon = $row_get_guide_headicon['uHeadPhoto'];
+                $sql_collect = "insert into collectedguide".
+                    "(cgGuideId,cgUserId,cgGuideRealName,cgGuideHeadIcon) ".
+                    "values($guideId,$userId,'$guideRealname','$guideHeadIcon')";
+                if ($conn->query($sql_collect) === TRUE) {
+                    Response::json(1,"Collect success","");
+                } else {
+                    Response::json(0,"Collect fail in server ".$conn->error,"");
+                }
+            }else{
+                Response::json(0,"Get guide head icon error: ".$conn->error,"");
             }
         }else{
-            Response::json(0,"Get guide head icon error: ".$conn->error,"");
+            Response::json(0,"Get guide information error: ".$conn->error,"");
         }
     }else{
-        Response::json(0,"Get guide information error: ".$conn->error,"");
+        Response::json(0,"You have collected this guide","");
     }
 }else{
     Response::json(0,"Account error","");
