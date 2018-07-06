0d0af5b1c77901d3e505b2af1c09c827f159aa48
lining <980994630@qq.com>
2018-07-06 16:35:06 +0800

    用户进行评论

--- Bowen/UserComments.php
+++ Bowen/UserComments.php
@@ -9,4 +9,26 @@
 require_once ('Connect.php');
 
 $tel = $_POST['tel'];
+$bowenId = $_POST['bowenId'];
+$content = $_POST['content'];
+$time = $_POST['time'];
 
+$sql_get_user_information = "select * from user where uTelephone='$tel'";
+$result_get_user_information = $conn->query($sql_get_user_information);
+if($result_get_user_information->num_rows>0){
+    $row_get_user_information = $result_get_user_information->fetch_assoc();
+    $userId = $row_get_user_information['uId'];
+    $userNickname = $row_get_user_information['uNickname'];
+    $userHeadIcon= $row_get_user_information['uHeadPhoto'];
+    $sql_comment = "insert into comment".
+                   "(cContent,cUserId,cUserNickName,cTime,cUserHeadIcon,cBowenId) ".
+                   "values('$content',$userId,'$userNickname','$time','$userHeadIcon',$bowenId)";
+    if ($conn->query($sql_comment) === TRUE) {
+        Response::json(1,"Comment bowen success","");
+    } else {
+        Response::json(0,"Comment fail 1 in server ".$conn->error,"");
+    }
+}else{
+    Response::json(0,"Account error","");
+}
+$conn->close();
