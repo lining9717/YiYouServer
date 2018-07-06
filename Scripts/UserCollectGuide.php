da1cbbec8703c0fb92578fc99d94f589de652d9f
lining <980994630@qq.com>
2018-07-06 15:09:40 +0800

    修改检测用户是否收藏向导是的错误

--- Scripts/UserCollectGuide.php
+++ Scripts/UserCollectGuide.php
@@ -25,35 +25,35 @@ $result_get_userId = $conn->query($sql_get_userId);
 if($result_get_userId->num_rows>0){
     $row_get_userId = $result_get_userId->fetch_assoc();
     $userId = $row_get_userId['uId'];
-    if(!isCollected($userId,$guideId,$conn)){
         $sql_get_guide_info = "select * from guide where gGuideNumber = '$guideNumber'";
         $result_get_guide_info = $conn->query($sql_get_guide_info);
         if($result_get_guide_info->num_rows>0){
             $row_get_guide_info = $result_get_guide_info->fetch_assoc();
             $guideId = $row_get_guide_info['gId'];
-            $guideRealname = $row_get_guide_info['gRealName'];
-            $sql_get_guide_headicon = "select uHeadPhoto from user where uGuideId=$guideId";
-            $result_get_guide_headicon = $conn->query($sql_get_guide_headicon);
-            if($result_get_guide_headicon->num_rows>0){
-                $row_get_guide_headicon = $result_get_guide_headicon->fetch_assoc();
-                $guideHeadIcon = $row_get_guide_headicon['uHeadPhoto'];
-                $sql_collect = "insert into collectedguide".
-                    "(cgGuideId,cgUserId,cgGuideRealName,cgGuideHeadIcon) ".
-                    "values($guideId,$userId,'$guideRealname','$guideHeadIcon')";
-                if ($conn->query($sql_collect) === TRUE) {
-                    Response::json(1,"Collect success","");
-                } else {
-                    Response::json(0,"Collect fail in server ".$conn->error,"");
+            if(!isCollected($userId,$guideId,$conn)){
+                $guideRealname = $row_get_guide_info['gRealName'];
+                $sql_get_guide_headicon = "select uHeadPhoto from user where uGuideId=$guideId";
+                $result_get_guide_headicon = $conn->query($sql_get_guide_headicon);
+                if($result_get_guide_headicon->num_rows>0){
+                    $row_get_guide_headicon = $result_get_guide_headicon->fetch_assoc();
+                    $guideHeadIcon = $row_get_guide_headicon['uHeadPhoto'];
+                    $sql_collect = "insert into collectedguide".
+                        "(cgGuideId,cgUserId,cgGuideRealName,cgGuideHeadIcon) ".
+                        "values($guideId,$userId,'$guideRealname','$guideHeadIcon')";
+                    if ($conn->query($sql_collect) === TRUE) {
+                        Response::json(1,"Collect success","");
+                    } else {
+                        Response::json(0,"Collect fail in server ".$conn->error,"");
+                    }
+                }else{
+                    Response::json(0,"Get guide head icon error: ".$conn->error,"");
                 }
             }else{
-                Response::json(0,"Get guide head icon error: ".$conn->error,"");
+                Response::json(0,"You have collected this guide","");
             }
         }else{
             Response::json(0,"Get guide information error: ".$conn->error,"");
         }
-    }else{
-        Response::json(0,"You have collected this guide","");
-    }
 }else{
     Response::json(0,"Account error","");
 }
